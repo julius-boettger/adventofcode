a6e00f922bf3069f71edd6c91e3a528a7e0048ee
julius-boettger <julius.btg@proton.me>
2024-12-01 22:07:52 +0100

    01.1: initial commit

--- 2024/src/01.cpp
+++ 2024/src/01.cpp
@@ -1,10 +1,53 @@
 #include "day.hpp"
 #if DAY == 1
 
+#include <format>
+#include <vector>
+#include <fstream>
+#include <sstream>
 #include <iostream>
+#include <algorithm>
+
+std::string readInput() {
+    std::ifstream inputFile { std::format("input/{:02}.txt", DAY) };
+    if (!inputFile.is_open()) {
+        throw;
+    }
+
+    // read whole file
+    std::string fileContents {
+        std::istreambuf_iterator<char>(inputFile),
+        std::istreambuf_iterator<char>()
+    };
+
+    inputFile.close();
+    return fileContents;
+}
+
 
 int main() {
-    std::cout << "day 1";
+    auto input { readInput() };
+
+    // split input into two lists
+    std::vector<int> list1, list2;
+    std::istringstream inputStream { input };
+    std::string line;
+    while (std::getline(inputStream, line)) {
+        // extract integers from line
+        std::istringstream lineStream { line };
+        int num1, num2;
+        lineStream >> num1 >> num2;
+        // add to lists
+        list1.push_back(num1);
+        list2.push_back(num2);
+    }
+
+    // sort lists
+    std::sort(list1.begin(), list1.end());
+    std::sort(list2.begin(), list2.end());
+
+    // loop through lists and compare
+
     return 0;
 }
 
