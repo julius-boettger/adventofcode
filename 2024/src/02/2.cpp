2686d593df90a15fb8b5ce07602eb5546293a8a9
julius-boettger <julius.btg@proton.me>
2024-12-03 18:42:36 +0100

    02.2: finish puzzle

--- 2024/src/02/2.cpp
+++ 2024/src/02/2.cpp
@@ -2,21 +2,15 @@
 #if DAY == 2 && PUZZLE == 2
 
 #include <cmath>
+#include <vector>
 #include <sstream>
 
-// return number of problems in line
-static int checkLine(std::string line) {
+// return number of problems in levels
+static int checkLevels(const std::vector<int>& levels) {
     auto problems { 0 };
 
-    // extract levels from line
-    std::istringstream lineStream { line };
-
-    int previousLevel;
-    lineStream >> previousLevel;
-
-    int level;
-    lineStream >> level;
-
+    int previousLevel { levels[0] };
+    int level { levels[1] };
     bool ascending { level > previousLevel };
 
     auto diff { std::abs(level - previousLevel) };
@@ -26,14 +20,15 @@ static int checkLine(std::string line) {
 
     previousLevel = level;
 
-    while (lineStream >> level) {
+    for (auto i { 2uz }; i < levels.size(); i++) {
+        level = levels[i];
         diff = std::abs(level - previousLevel);
         if (diff > 3 ||
             diff < 1 ||
             ascending != (level > previousLevel))
         {
             problems++;
-            break; 
+            break;
         }
 
         ascending = level > previousLevel;
@@ -50,9 +45,29 @@ void solution(std::string input) {
     std::istringstream inputStream { input };
     std::string line;
     while (std::getline(inputStream, line)) {
-        auto problems { checkLine(line) };
+        // extract levels from line
+        std::vector<int> levels {};
+        std::istringstream lineStream { line };
+        int level;
+        while (lineStream >> level) {
+            levels.push_back(level);
+        }
+
+        auto problems { checkLevels(levels) };
         if (problems == 0) {
             total_safe_reports++;
+        } else if (problems <= 2) {
+            // try if it's safe when removing an element
+            // (not optimal, the number of loop iterations could be reduced)
+            for (auto i { 0uz }; i < levels.size(); i++) {
+                auto modified_levels { levels };
+                modified_levels.erase(modified_levels.begin() + static_cast<int>(i));
+                problems = checkLevels(modified_levels);
+                if (problems == 0) {
+                    total_safe_reports++;
+                    break;
+                }
+            }
         }
     }
 
