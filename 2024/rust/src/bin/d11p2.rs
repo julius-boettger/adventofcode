43aa15d23fda0f417df19d3ae056a2a96e488154
julius-boettger <julius.btg@proton.me>
2024-12-13 00:10:28 +0100

    11.2: improve performance by caching stone moves

--- 2024/rust/src/bin/d11p2.rs
+++ 2024/rust/src/bin/d11p2.rs
@@ -1,11 +1,10 @@
+use std::collections::HashMap;
+
 type Stone = u64;
 type StoneQuantity = u64;
 
-#[derive(Debug)]
-struct StoneWrap {
-    quantity: StoneQuantity,
-    stone: Stone,
-}
+type StoneMap = HashMap<Stone, StoneQuantity>;
+type MovementCache = HashMap<Stone, Vec<Stone>>;
 
 fn blink_at_stone(stone: &Stone) -> Vec<Stone> {
     if *stone == 0 {
@@ -25,42 +24,53 @@ fn blink_at_stone(stone: &Stone) -> Vec<Stone> {
     vec![stone * 2024]
 }
 
-fn blink_at_stones(stones: Vec<StoneWrap>) -> Vec<StoneWrap> {
-    let mut moved_wraps: Vec<StoneWrap> = vec![];
-    // for every given stone wrap
-    for start_wrap in stones {
-        // for every moved stone after blinking at it
-        for moved_stone in blink_at_stone(&start_wrap.stone) {
-            // check if it's already in the vec
-            match moved_wraps.iter_mut().find(|wrap| wrap.stone == moved_stone) {
-                // add it to the vec
-                None => {
-                    moved_wraps.push(StoneWrap {
-                        quantity: start_wrap.quantity,
-                        stone: moved_stone
-                    });
-                }
-                // add it's quantity to the existing element in the vec
-                Some(moved_wrap) => {
-                    moved_wrap.quantity += start_wrap.quantity;
-                }
+fn blink_at_stone_cached(stone: &Stone, movemement_cache: &mut MovementCache) -> Vec<Stone> {
+    // check if stone to blink at is already in cache
+    match movemement_cache.get(stone) {
+        // return cached result
+        Some(result) => result.clone(),
+        // compute result, push to cache and return
+        None => {
+            let result = blink_at_stone(stone);
+            movemement_cache.insert(*stone, result.clone());
+            result
+        }
+    }
+}
+
+fn blink_at_stones(stone_map: StoneMap, movemement_cache: &mut MovementCache) -> StoneMap {
+    let mut new_stone_map: StoneMap = HashMap::new();
+    // for every given stone and its quantity
+    for stone_pair in stone_map {
+        // for every new stone after blinking at it
+        for new_stone in blink_at_stone_cached(&stone_pair.0, movemement_cache) {
+            // check if it's already in the map
+            match new_stone_map.get_mut(&new_stone) {
+                // add it's quantity to the existing pair in the map
+                Some(quantity) => { *quantity += stone_pair.1; }
+                // add it to the map
+                None => { new_stone_map.insert(new_stone, stone_pair.1); }
             }
         }
     }
-    moved_wraps
+    new_stone_map
 }
 
 fn main() {
     let mut input = advent_of_code::input!();
     input = input.replace("\n", "");
-    let mut stones: Vec<StoneWrap> = input
+
+    // key: number engraved on stone
+    // value: quantity
+    let mut stone_map: StoneMap = input
         .split(" ")
-        .map(|s| StoneWrap { quantity: 1, stone: s.parse().unwrap() })
+        .map(|s| (s.parse().unwrap(), 1))
         .collect();
 
+    let mut stone_movement_cache: MovementCache = HashMap::new();
     for _ in 0..75 {
-        stones = blink_at_stones(stones);
+        stone_map = blink_at_stones(stone_map, &mut stone_movement_cache);
     }
 
-    println!("{}", stones.into_iter().map(|s| s.quantity).sum::<StoneQuantity>());
+    println!("{}", stone_map.values().sum::<StoneQuantity>());
 }
