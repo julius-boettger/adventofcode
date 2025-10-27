c07c2db881e0143c66d65ab2c40851b9d8fcd324
julius-boettger <julius.btg@proton.me>
2025-10-27 21:56:36 +0100

    nix: add flake for rust

---
+++ rust/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    systems.url = "github:nix-systems/default"; # can run on all systems
+  };
+
+  outputs = { self, nixpkgs, systems, ... }:
+  let
+    eachSystem = fn: nixpkgs.lib.genAttrs (import systems) (system: fn system (import nixpkgs {
+      inherit system;
+    }));
+  in
+  {
+    devShells = eachSystem (system: pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          clippy
+          cargo-edit # provides `cargo upgrade` for dependencies
+        ];
+        # fix rust-analyzer in vscode
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    });
+  };
+}
