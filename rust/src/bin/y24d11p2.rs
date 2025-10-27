084414e5a93a4fd6af3524b64a5f58af41653948
julius-boettger <julius.btg@proton.me>
2025-10-27 22:19:51 +0100

    24/11.2: fix clippy warnings

--- rust/src/bin/y24d11p2.rs
+++ rust/src/bin/y24d11p2.rs
@@ -6,8 +6,8 @@ type StoneQuantity = u64;
 type StoneMap = HashMap<Stone, StoneQuantity>;
 type MovementCache = HashMap<Stone, Vec<Stone>>;
 
-fn blink_at_stone(stone: &Stone) -> Vec<Stone> {
-    if *stone == 0 {
+fn blink_at_stone(stone: Stone) -> Vec<Stone> {
+    if stone == 0 {
         return vec![1];
     }
 
@@ -24,17 +24,15 @@ fn blink_at_stone(stone: &Stone) -> Vec<Stone> {
     vec![stone * 2024]
 }
 
-fn blink_at_stone_cached(stone: &Stone, movemement_cache: &mut MovementCache) -> Vec<Stone> {
+fn blink_at_stone_cached(stone: Stone, movemement_cache: &mut MovementCache) -> Vec<Stone> {
     // check if stone to blink at is already in cache
-    match movemement_cache.get(stone) {
-        // return cached result
-        Some(result) => result.clone(),
-        // compute result, push to cache and return
-        None => {
-            let result = blink_at_stone(stone);
-            movemement_cache.insert(*stone, result.clone());
-            result
-        }
+    #[allow(clippy::option_if_let_else)]
+    if let Some(result) = movemement_cache.get(&stone) {
+        result.clone()
+    } else {
+        let result = blink_at_stone(stone);
+        movemement_cache.insert(stone, result.clone());
+        result
     }
 }
 
@@ -43,12 +41,13 @@ fn blink_at_stones(stone_map: StoneMap, movemement_cache: &mut MovementCache) ->
     // for every given stone and its quantity
     for stone_pair in stone_map {
         // for every new stone after blinking at it
-        for new_stone in blink_at_stone_cached(&stone_pair.0, movemement_cache) {
+        for new_stone in blink_at_stone_cached(stone_pair.0, movemement_cache) {
             // check if it's already in the map
             match new_stone_map.get_mut(&new_stone) {
                 // add it's quantity to the existing pair in the map
                 Some(quantity) => { *quantity += stone_pair.1; }
                 // add it to the map
+                #[allow(non_snake_case)]
                 None => { new_stone_map.insert(new_stone, stone_pair.1); }
             }
         }
@@ -58,12 +57,12 @@ fn blink_at_stones(stone_map: StoneMap, movemement_cache: &mut MovementCache) ->
 
 fn main() {
     let mut input = advent_of_code::input!();
-    input = input.replace("\n", "");
+    input = input.replace('\n', "");
 
     // key: number engraved on stone
     // value: quantity
     let mut stone_map: StoneMap = input
-        .split(" ")
+        .split(' ')
         .map(|s| (s.parse().unwrap(), 1))
         .collect();
 
