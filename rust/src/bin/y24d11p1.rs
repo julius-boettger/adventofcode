e0706d831a3491a8b5d6c110799fa420998683ae
julius-boettger <julius.btg@proton.me>
2025-10-29 11:40:15 +0100

    24/11.1: optimize runtime

--- rust/src/bin/y24d11p1.rs
+++ rust/src/bin/y24d11p1.rs
@@ -1,18 +1,31 @@
 type Stone = u64;
 
+const fn decimal_digits(mut stone: Stone) -> u8 {
+    if stone == 0 {
+        return 1;
+    }
+
+    let mut count = 0;
+    while stone > 0 {
+        stone /= 10;
+        count += 1;
+    }
+    count
+}
+
 fn blink_at_stone(stone: Stone) -> Vec<Stone> {
     if stone == 0 {
         return vec![1];
     }
 
-    let stone_string = stone.to_string();
-    let stone_string_len = stone_string.len();
-    if stone_string.len() % 2 == 0 {
-        let stones = stone_string.split_at(stone_string_len / 2);
-        return vec![
-            stones.0.parse().unwrap(),
-            stones.1.parse().unwrap()
-        ];
+    let stone_digits = decimal_digits(stone);
+    if stone_digits % 2 == 0 {
+        // "1234" => ["12", "34"]
+        // power of 10 that devides the two halves of digits
+        let split_indicator: Stone = Stone::from(10u8).pow((stone_digits / 2).into());
+        let stone_1 = stone / split_indicator;
+        let stone_2 = stone - (stone_1 * split_indicator);
+        return vec![stone_1, stone_2];
     }
 
     vec![stone * 2024]
