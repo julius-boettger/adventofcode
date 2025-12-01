9522936ffdccf4afe7c8c905255decb26c6e1c18
julius-boettger <julius.btg@proton.me>
2025-12-01 11:23:37 +0100

    25/01.1: initial commit

---
+++ rust/src/bin/y25d01p1.rs
@@ -0,0 +1,26 @@
+// doesnt work yet, answer is too low, dont know why, example input works
+
+#[advent_of_code::main("25/01")]
+fn main() {
+    let rotations = INPUT.lines()
+        .map(|line| {
+            let direction_factor = if line.starts_with('L') { -1 } else { 1 };
+            let distance: i16 = line[1..].parse().unwrap();
+            distance * direction_factor
+        })
+        .collect::<Vec<_>>();
+    
+    let mut password: u16 = 0;
+    let mut position = 50;
+    for rotation in rotations {
+        position += rotation;
+        if position > 99 { position -= 100; }
+        if position <  0 { position += 100; }
+        
+        if position == 0 {
+            password += 1;
+        }
+    }
+
+    println!("{password}");
+}
