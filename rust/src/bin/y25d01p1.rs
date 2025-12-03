ae2f6eca549c00421015bb1c19daf368d3172237
julius-boettger <julius.btg@proton.me>
2025-12-04 00:20:31 +0100

    25/01.1: refactor/optimize

--- rust/src/bin/y25d01p1.rs
+++ rust/src/bin/y25d01p1.rs
@@ -1,17 +1,14 @@
 #[advent_of_code::main("25/01")]
 fn main() {
-    let rotations = INPUT.lines()
-        .map(|line| {
-            let direction_factor = if line.starts_with('L') { -1 } else { 1 };
-            let distance: i16 = line[1..].parse().unwrap();
-            distance * direction_factor
-        })
-        .collect::<Vec<_>>();
-    
     let mut password: u16 = 0;
     let mut position = 50;
-    for rotation in rotations {
-        position += rotation;
+
+    for line in INPUT.lines() {
+        let direction_factor = if line.starts_with('L') { -1 } else { 1 };
+        let distance: i16 = line[1..].parse().unwrap();
+
+        position += distance * direction_factor;
+
         if position % 100 == 0 {
             password += 1;
         }
