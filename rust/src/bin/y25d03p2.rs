0736ab5094f1f1b458cf739465bb8a71bca31210
julius-boettger <julius.btg@proton.me>
2025-12-03 23:06:17 +0100

    25/03.2: finish puzzle

--- rust/src/bin/y25d03p2.rs
+++ rust/src/bin/y25d03p2.rs
@@ -1,20 +1,37 @@
-// doesnt work yet, wrong on example input.
-// joltage calculation is correct, battery_bytes selection loop is wrong.
-
 type JoltageSum = u64;
 
 /// batteries per pank to turn on
 const BATS: usize = 12;
+
+fn max_byte(bank: &str) -> (u8, usize) {
+    let mut max_byte = 0;
+    let mut max_pos = 0;
+    for (pos, byte) in bank.bytes().enumerate() {
+        if byte > max_byte {
+            max_byte = byte;
+            max_pos = pos;
+        }
+    }
+    (max_byte, max_pos)
+}
 
 #[advent_of_code::main("25/03")]
 fn main() {
     println!("{}", INPUT.lines()
         .map(|bank| {
             let mut battery_bytes = [0; BATS];
-            battery_bytes[BATS-1] = bank[..bank.len() - (BATS-1)].bytes().max().unwrap();
+            let mut battery_positions = [0; BATS];
+
+            let (first_byte, first_byte_pos) = max_byte(&bank[..bank.len() - (BATS-1)]);
+            battery_bytes[BATS-1] = first_byte;
+            battery_positions[BATS-1] = first_byte_pos;
+
             for i in (0..(BATS-1)).rev() {
-                let last_pos = bank.bytes().position(|b| b == *battery_bytes.last().unwrap()).unwrap();
-                battery_bytes[i] = bank[last_pos+1 .. bank.len()-i].bytes().max().unwrap();
+                let last_pos = battery_positions[i+1];
+                let relevant_bank = &bank[last_pos+1 .. bank.len()-i];
+                let (byte, pos) = max_byte(relevant_bank);
+                battery_bytes[i] = byte;
+                battery_positions[i] = pos + (last_pos+1);
             }
             
             let joltage = battery_bytes.into_iter()
