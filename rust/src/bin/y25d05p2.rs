eeccfc6aaf0edb612e190386bfc6a0969ebb59ed
julius-boettger <julius.btg@proton.me>
2025-12-06 20:37:16 +0100

    25/05.2: finish puzzle

--- rust/src/bin/y25d05p2.rs
+++ rust/src/bin/y25d05p2.rs
@@ -1,16 +1,38 @@
-// doesnt work yet
-// try with example input
-// dont only check if the start of a range is contained in another range,
-// also the end also needs to be greater than the start, otherwise they dont overlap
-
 type ID = u64;
+type IDRange = std::ops::RangeInclusive<ID>;
 
-fn any_contain(ranges: &[std::ops::RangeInclusive<ID>], id: ID) -> Option<usize> {
-    for (i, range) in ranges.iter().enumerate() {
-        if range.contains(&id) {
-            return Some(i);
+/// check if a `new_range` can be represented by merging it with an existing range.
+/// if so, return the index and the new value of the range to modify.
+fn mergeable(ranges: &[IDRange], new_range: &IDRange, skip_i: Option<usize>) -> Option<(usize, IDRange)> {
+    for (i, iter_range) in ranges.iter().enumerate() {
+        if let Some(skip_i) = skip_i && i == skip_i {
+            continue;
+        }
+
+        if iter_range == new_range {
+            return Some((i, iter_range.clone()));
+        }
+
+        // check both possible orders
+        for (range_1, range_2) in [(iter_range, new_range), (new_range, iter_range)] {
+            if !range_1.contains(range_2.start()) {
+                continue;
+            }
+
+            // range_1: [-----]
+            // range_2:  [---]
+            if range_1.contains(range_2.end()) {
+                return Some((i, range_1.clone()));
+            }
+
+            // range_1: [---]
+            // range_2:   [---]
+            if range_2.contains(range_1.end()) {
+                return Some((i, *range_1.start() ..= *range_2.end()));
+            }
         }
     }
+
     None
 }
 
@@ -23,37 +45,46 @@ fn main() {
         let (start, end) = line.split_once('-').unwrap();
         let start = start.parse::<ID>().unwrap();
         let end = end.parse::<ID>().unwrap();
+        let range = start ..= end;
 
-        if let Some(i) = any_contain(&valid_ranges, start) {
-            let start = *valid_ranges[i].start();
-            let end = *valid_ranges[i].end().max(&end);
-            valid_ranges[i] = start ..= end;
+        if let Some((i, modified_range)) = mergeable(&valid_ranges, &range, None) {
+            valid_ranges[i] = modified_range;
         } else {
-            valid_ranges.push(start ..= end);
+            valid_ranges.push(range);
         }
     }
 
     valid_ranges.sort_unstable_by_key(|range| *range.end());
 
-    println!("{:?}", valid_ranges[0]);
-    println!("{:?}", valid_ranges[valid_ranges.len()-1]);
+    'outer: loop {
+        let mut any_modified = false;
 
-    for i in (0..valid_ranges.len()).rev() {
-        let range = &valid_ranges[i];
-        if let Some(contained_i) = any_contain(&valid_ranges, *range.start()) {
-            let start = *valid_ranges[contained_i].start();
-            let end = *valid_ranges[contained_i].end().max(range.end());
-            valid_ranges[contained_i] = start ..= end;
+        for iter_i in (0..valid_ranges.len()).rev() {
+            let mergeable = mergeable(&valid_ranges, &valid_ranges[iter_i], Some(iter_i));
+            let Some((modify_i, modified_range)) = mergeable else {
+                continue;
+            };
+
+            valid_ranges[modify_i] = modified_range;
+            any_modified = true;
             // should be fine because we iterate in reverse,
             // swapped elements have already been checked
             //valid_ranges.swap_remove(i);
-            //valid_ranges.remove(i);
+            valid_ranges.remove(iter_i);
 
             if valid_ranges.len() == 1 {
-                break;
+                break 'outer;
             }
+        }
+
+        if !any_modified {
+            break;
         }
     }
 
-    println!("{valid_ranges:?}", );
+    let valid_ids = valid_ranges.into_iter()
+        .map(|range| range.end() - range.start() + 1)
+        .sum::<ID>();
+
+    println!("{valid_ids}");
 }
