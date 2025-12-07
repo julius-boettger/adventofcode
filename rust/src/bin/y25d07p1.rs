6afa76a38e451bf450e5f14661897dd683ca6ee6
julius-boettger <julius.btg@proton.me>
2025-12-07 13:41:28 +0100

    25/07.1: optimize

--- rust/src/bin/y25d07p1.rs
+++ rust/src/bin/y25d07p1.rs
@@ -1,10 +1,11 @@
-type SplitterSet = std::collections::HashSet<(usize, usize)>;
+type Index = u8;
+type CoordSet = std::collections::HashSet<(Index, Index)>;
 
-fn get_char(lines: &[&'static str], row: usize, col: usize) -> Option<char> {
-   Some(char::from(lines.get(row)?.as_bytes()[col]))
+fn get_char(lines: &[&'static str], row: Index, col: Index) -> Option<char> {
+   Some(char::from(lines.get(row as usize)?.as_bytes()[col as usize]))
 }
 
-fn simulate_beam(lines: &[&'static str], start_row: usize, col: usize, splitter_set: &mut SplitterSet) {
+fn simulate_beam(lines: &[&'static str], start_row: Index, col: Index, activated_splitters: &mut CoordSet) {
     let mut current_row = start_row;
     loop {
         let Some(c) = get_char(lines, current_row, col) else {
@@ -13,23 +14,23 @@ fn simulate_beam(lines: &[&'static str], start_row: usize, col: usize, splitter_
         if c != '.' {
             break;
         }
-        current_row += 1;
+        current_row += 2;
     }
 
-    if !splitter_set.insert((current_row, col)) {
+    if !activated_splitters.insert((current_row, col)) {
         // the path of this splitter has already been checked
         return;
     }
 
-    simulate_beam(lines, current_row, col - 1, splitter_set);
-    simulate_beam(lines, current_row, col + 1, splitter_set);
+    simulate_beam(lines, current_row + 2, col - 1, activated_splitters);
+    simulate_beam(lines, current_row + 2, col + 1, activated_splitters);
 }
 
 #[advent_of_code::main("25/07")]
 fn main() {
     let input_lines = INPUT.lines().collect::<Vec<_>>();
-    let start_col = input_lines[0].chars().position(|c| c == 'S').unwrap();
-    let mut splitter_set = SplitterSet::new();
-    simulate_beam(&input_lines, 0, start_col, &mut splitter_set);
-    println!("{}", splitter_set.len());
+    let start_col = Index::try_from(input_lines[0].chars().position(|c| c == 'S').unwrap()).unwrap();
+    let mut activated_splitters = CoordSet::new();
+    simulate_beam(&input_lines, 2, start_col, &mut activated_splitters);
+    println!("{}", activated_splitters.len());
 }
