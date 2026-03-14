3df2945d96a8d6b045088af99c0057e30e9ed821
julius-boettger <julius.btg@proton.me>
2026-03-14 14:05:25 +0100

    24/22.1: optimize with simd

--- rust/solutions/src/bin/y24d22p1.rs
+++ rust/solutions/src/bin/y24d22p1.rs
@@ -1,20 +1,28 @@
+#![feature(portable_simd)]
+
 use rayon::prelude::*;
+use std::simd::prelude::*;
 
 type Num = u64;
 
-const fn next_secret_number(secret_number: Num) -> Num {
+const LANES: usize = 16;
+type SimdNum = Simd<u64, LANES>;
+
+fn next_secret_number(secret_number: SimdNum) -> SimdNum {
+    const AND_MASK: SimdNum = SimdNum::from_array([0x00FF_FFFF; LANES]);
+
     let mut result = secret_number;
     result ^= result << 6;
-    result &= 0x00FF_FFFF;
+    result &= AND_MASK;
     result ^= result >> 5;
-    result &= 0x00FF_FFFF;
+    result &= AND_MASK;
     result ^= result << 11;
-    result &= 0x00FF_FFFF;
+    result &= AND_MASK;
     result
 }
 
-fn next_nth_secret_number(secret_number: Num) -> Num {
-    let mut result = secret_number;
+fn next_nth_secret_number(simd_num: SimdNum) -> SimdNum {
+    let mut result = simd_num;
     for _ in 0 .. 2000 {
         result = next_secret_number(result);
     }
@@ -23,9 +31,22 @@ fn next_nth_secret_number(secret_number: Num) -> Num {
 
 #[advent_of_code::main("24/22")]
 fn main() {
-    println!("{}", INPUT
-        .par_lines()
-        .map(|line| next_nth_secret_number(line.parse::<Num>().unwrap()))
+    let lines = INPUT.lines().collect::<Vec<_>>();
+    println!("{}", lines.par_chunks(LANES)
+        .map(|chunk| {
+            let mut parsed = chunk.par_iter()
+                .map(|line| line.parse::<Num>().unwrap())
+                .collect::<Vec<_>>();
+
+            // make sure every chunk has LANES values
+            if parsed.len() != LANES {
+                parsed.resize(LANES, 0);
+            }
+
+            let simd_num = SimdNum::from_slice(&parsed);
+            let simd_result = next_nth_secret_number(simd_num);
+            simd_result.reduce_sum()
+        })
         .sum::<Num>()
     );
 }
