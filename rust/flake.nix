7d514158aab45b9b331c3f6c29728120fbb1a5cb
julius-boettger <julius.btg@proton.me>
2026-03-14 14:03:25 +0100

    nix: use nightly rust

--- rust/flake.nix
+++ rust/flake.nix
@@ -2,27 +2,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     systems.url = "github:nix-systems/default"; # can run on all systems
+    rust-overlay = { url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = { self, nixpkgs, systems, ... }:
+  outputs = { self, nixpkgs, systems, rust-overlay, ... }:
   let
+    overlays = [ rust-overlay.overlays.default ];
     eachSystem = fn: nixpkgs.lib.genAttrs (import systems) (system: fn system (import nixpkgs {
-      inherit system;
+      inherit system overlays;
     }));
   in
   {
     devShells = eachSystem (system: pkgs: {
       default = pkgs.mkShell {
         packages = with pkgs; [
-          rustc cargo # rust
-          clippy # better linter
+          # nightly rust for experimental features
+          (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+            # fix rust-analyzer in vscode
+            extensions = [ "rust-src" ];
+          }))
+
           cargo-edit # provides `cargo upgrade` for dependencies
           samply # profiler
           hyperfine # benchmarking
           graphviz # graph visualization
         ];
-        # fix rust-analyzer in vscode
-        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
       };
     });
   };
